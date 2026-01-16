4e205141ef141ba082423cfda2b40ab2280ab3c5
mayankhansraj12 <mayankhansraj12@gmail.com>
2026-01-16 19:04:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://bnb-ptsa.onrender.com",
     credentials:true
 }))
 
