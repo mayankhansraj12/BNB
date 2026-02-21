576b600b931e7c73b650e544028621d98c27fc5f
Mayank Hansraj <mayankhansraj12@gmail.com>
2026-02-21 18:00:30 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,17 @@ import listingRouter from "./routes/listing.route.js";
 import bookingRouter from "./routes/booking.route.js";
 
 let port = process.env.PORT || 8000;
+
+connectDb();
+
+let app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
 );
 
 app.use("/api/auth", authRouter);
